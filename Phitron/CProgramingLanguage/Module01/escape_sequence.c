1683c502c95c349516b6546815bbd2bc12ef37b1
asifraiyan-systems <asifraiyan.systems@gmail.com>
2026-04-10 16:59:35 +0600

    C Programmng Language Escape Sequence

---
+++ Phitron/CProgramingLanguage/Module01/escape_sequence.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    printf("I Love Programmin.\nI Love Bangladesh\n");
+    printf("I Love Progr\tammin\n");
+    printf("I Love Progr\bammin\n");
+    printf("\"C\" Programming Language\n");
+    printf("\'C\' Programming Language\n");
+    printf("\\C\\ Programming Language\n");
+    printf("C\r Programming Language\n");
+    printf("C\f Programming Language\n");
+    printf("C\a Programming Language\n");
+    printf("C\v Programming Language\n");
+    printf("Are you Bangladeshi\?\n");
+    printf("C\0 Programming Language\n");
+
+    return 0;
+}
