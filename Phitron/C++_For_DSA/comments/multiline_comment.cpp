eeb2fb041d8537e3e00222a72d63af47f9134112
asifraiyan-systems <asifraiyan.systems@gmail.com>
2026-05-24 00:03:08 +0600

    feat: add basic C++ syntax and I/O practice

---
+++ Phitron/C++_For_DSA/comments/multiline_comment.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int age = 26;
+
+    cout << "Age: " << age;
+    /* This is multiline comment.
+    I Love Programming Language */
+
+    return 0;
+}
---
+++ Phitron/C++_For_DSA/comments/single_line_comment.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    // This is sigle line comment
+
+    int age = 26;
+
+    cout << age;
+
+    return 0;
+}
+
+ 
